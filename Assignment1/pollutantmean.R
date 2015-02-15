3e5452c010830890d367ca475af2c1cfccd107b8
Venkatesh HariRama Subbu <venkat_h@hotmail.com>
2015-02-15 13:36:14 -0500

    R programming

---
+++ Assignment1/pollutantmean.R
@@ -0,0 +1,25 @@
+pollutantmean <- function (directory,pollutant,id) 
+{	
+	##Intialize the vector
+	data <- c()
+
+	##loop for the id vecto
+	for (integer in id)
+	{	
+		##file location
+		file_name <- paste(integer ,"csv",sep =".")
+		full_directory <- paste(directory,file_name,sep ="/")
+		
+		x <- read.csv(full_directory)
+		
+		##getting particular column value 
+		y<- x[,pollutant]
+		bad<-is.na(y)
+		y<-y[!bad]
+		
+		##appending all data into single vector
+		data <- c(data, y)
+	}
+	##mean and rouding value
+	round(mean(data),3)
+}
---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+## Course Era 
+##Data Science
+ 
+##R Programming 
+
+
+The repo covers the three assignment coding, sample data and testing cases
